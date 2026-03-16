61ab6990f7aab21dd43643726f818b3d755e8aeb
DUVAL Arnaud <arnaud.duval@insa-lyon.fr>
2026-03-16 10:52:05 +0100

    Update settings management.

--- src/consensio/consensio/settings.py
+++ src/consensio/consensio/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b@e)p=ru4yh1hq$kytyup1i59_bw$n6#ep$lt58%-uolgd&*=9'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
